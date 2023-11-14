13ed3cadc75ea0669c706d7fd2323d1fc4b4de67
Rafael Senzini <rafael_senziani@hotmail.com>
2023-11-13 22:11:21 -0300

    First Commit

---
+++ testeweb/src/main/java/com/example/testeweb/testeweb/TestewebApplication.java
@@ -0,0 +1,13 @@
+package com.example.testeweb.testeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestewebApplication.class, args);
+	}
+
+}
---
+++ testeweb/src/test/java/com/example/testeweb/testeweb/TestewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeweb.testeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
